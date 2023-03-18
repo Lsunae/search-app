1150f758bfa79c6ce732edc071b9fdbcc224f833
Lsunae <sjsao5012@gmail.com>
2023-03-19 02:17:30 +0900

    리스너 셋팅 함수 추가

--- app/src/main/java/com/lsunae/search_app/view/search/SearchFragment.kt
+++ app/src/main/java/com/lsunae/search_app/view/search/SearchFragment.kt
@@ -1,13 +1,14 @@
-package com.lsunae.search_app.view.search
-
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lsunae.search_app.R
 import com.lsunae.search_app.databinding.FragmentSearchBinding
+import com.lsunae.search_app.util.OnSingleClickListener
 import com.lsunae.search_app.view.base.BaseFragment
 import com.lsunae.search_app.view.search.adapter.SearchResultAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     private lateinit var searchResultAdapter: SearchResultAdapter
 
@@ -15,12 +16,22 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
         super.onViewCreated(view, savedInstanceState)
 
         setAdapter()
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.apply {
+            ivSearch.setOnClickListener(object : OnSingleClickListener() {
+                override fun onSingleClick(v: View) {
+                    val searchText = etSearch.text.toString()
+                }
+            })
+        }
     }
 
     private fun setAdapter() {
         searchResultAdapter = SearchResultAdapter()
         binding.rvSearchResult.apply {
-            // 기존 코드
             layoutManager = LinearLayoutManager(requireContext())
             adapter = searchResultAdapter
         }
