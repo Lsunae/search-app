a46090ec88d07c477a8c5c4c108584087f770860
Lsunae <sjsao5012@gmail.com>
2023-03-18 17:43:00 +0900

    SearchFragment 생성

---
+++ app/src/main/java/com/lsunae/search_app/view/search/SearchFragment.kt
@@ -0,0 +1,9 @@
+package com.lsunae.search_app.view.search
+
+import com.lsunae.search_app.R
+import com.lsunae.search_app.databinding.FragmentSearchBinding
+import com.lsunae.search_app.view.base.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+}
