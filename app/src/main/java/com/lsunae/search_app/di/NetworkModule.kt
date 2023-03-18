367b78200b950a9fa9e74826ffa6187f37662bf0
Lsunae <sjsao5012@gmail.com>
2023-03-19 02:13:54 +0900

    NetworkModule 구현

---
+++ app/src/main/java/com/lsunae/search_app/di/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.lsunae.search_app.di
+
+import com.lsunae.search_app.api.ImageSearchService
+import com.lsunae.search_app.util.Constants.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+        return Retrofit.Builder()
+            .client(client)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageSearchService(
+        retrofit: Retrofit
+    ): ImageSearchService {
+        return retrofit.create(ImageSearchService::class.java)
+    }
+}
