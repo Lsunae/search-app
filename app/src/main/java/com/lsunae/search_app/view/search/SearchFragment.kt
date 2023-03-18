151057a9aa256655d9f3037840ff0c25ed6479a8
Lsunae <sjsao5012@gmail.com>
2023-03-18 18:40:51 +0900

    검색화면 구성 및 어댑터 적용

--- app/src/main/java/com/lsunae/search_app/view/search/SearchFragment.kt
+++ app/src/main/java/com/lsunae/search_app/view/search/SearchFragment.kt
@@ -1,9 +1,28 @@
 package com.lsunae.search_app.view.search
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lsunae.search_app.R
 import com.lsunae.search_app.databinding.FragmentSearchBinding
 import com.lsunae.search_app.view.base.BaseFragment
+import com.lsunae.search_app.view.search.adapter.SearchResultAdapter
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    private lateinit var searchResultAdapter: SearchResultAdapter
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        searchResultAdapter = SearchResultAdapter()
+        binding.rvSearchResult.apply {
+            // 기존 코드
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = searchResultAdapter
+        }
+    }
 }
