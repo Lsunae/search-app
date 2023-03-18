454acea8ac370fabfc320bd9860d1ced6a31352a
Lsunae <sjsao5012@gmail.com>
2023-03-18 17:43:25 +0900

    StorageFragment 생성

---
+++ app/src/main/java/com/lsunae/search_app/view/storage/StorageFragment.kt
@@ -0,0 +1,9 @@
+package com.lsunae.search_app.view.storage
+
+import com.lsunae.search_app.R
+import com.lsunae.search_app.databinding.FragmentStorageBinding
+import com.lsunae.search_app.view.base.BaseFragment
+
+class StorageFragment : BaseFragment<FragmentStorageBinding>(R.layout.fragment_storage) {
+
+}
