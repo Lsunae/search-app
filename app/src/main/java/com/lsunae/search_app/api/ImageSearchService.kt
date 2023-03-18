8c2c399272b1f1bf5a7f73c9d64c113893ef939f
Lsunae <sjsao5012@gmail.com>
2023-03-19 02:06:38 +0900

    이미지 검색 api 추가

---
+++ app/src/main/java/com/lsunae/search_app/api/ImageSearchService.kt
@@ -0,0 +1,19 @@
+package com.lsunae.search_app.api
+
+import com.lsunae.search_app.data.model.ImageSearchResponse
+import com.lsunae.search_app.util.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface ImageSearchService {
+    @GET("v2/search/image")
+    suspend fun searchImage(
+        @Header("Authorization") apiKey: String = Constants.AUTH_HEADER,
+        @Query("query") query: String,
+        @Query("sort") sort: String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): Response<ImageSearchResponse>
+}
