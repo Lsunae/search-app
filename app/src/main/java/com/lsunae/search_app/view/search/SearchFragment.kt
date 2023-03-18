40ea5e1d1c875ac3ab4371a316fc8555aaf92744
Lsunae <sjsao5012@gmail.com>
2023-03-19 02:24:34 +0900

    package 경로 추가 수정

--- app/src/main/java/com/lsunae/search_app/view/search/SearchFragment.kt
+++ app/src/main/java/com/lsunae/search_app/view/search/SearchFragment.kt
@@ -1,15 +1,20 @@
+package com.lsunae.search_app.view.search
+
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lsunae.search_app.R
 import com.lsunae.search_app.databinding.FragmentSearchBinding
 import com.lsunae.search_app.util.OnSingleClickListener
 import com.lsunae.search_app.view.base.BaseFragment
 import com.lsunae.search_app.view.search.adapter.SearchResultAdapter
+import com.lsunae.search_app.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    private val viewModel: SearchViewModel by viewModels()
     private lateinit var searchResultAdapter: SearchResultAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
