bea1eb0f64edc76915396e388c81345974ed5b35
Lsunae <sjsao5012@gmail.com>
2023-03-20 03:21:41 +0900

    검색 결과 데이터 변수 수정

--- app/src/main/java/com/lsunae/search_app/data/model/SearchResultData.kt
+++ app/src/main/java/com/lsunae/search_app/data/model/SearchResultData.kt
@@ -3,9 +3,7 @@ package com.lsunae.search_app.data.model
 import java.util.*
 
 data class SearchResultData(
-    val imageThumbnail: String?,
-    val videoThumbnail: String?,
-    val imageDateTime: Date?,
-    val videoDateTime: Date?,
+    val thumbnail: String?,
+    val dateTime: Date?,
     val urlType: String
 )
