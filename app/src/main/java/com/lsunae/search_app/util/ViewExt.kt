db5c3d0fc7b8409e6e2361756c9723a95c18da03
Lsunae <sjsao5012@gmail.com>
2023-03-20 00:28:04 +0900

    키보드 내리기 추가

--- app/src/main/java/com/lsunae/search_app/util/ViewExt.kt
+++ app/src/main/java/com/lsunae/search_app/util/ViewExt.kt
@@ -1,5 +1,8 @@
 package com.lsunae.search_app.util
 
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.lsunae.search_app.R
@@ -12,4 +15,12 @@ fun ImageView.glideImageSet(image: String) {
         .override(width, height)
         .centerCrop()
         .into(this)
+}
+
+fun View.hideKeyboard() {
+    val manager = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
+    manager?.hideSoftInputFromWindow(
+        windowToken,
+        InputMethodManager.HIDE_NOT_ALWAYS
+    )
 }
