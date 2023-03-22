52d2259905a7e185b9fe1fae884ea0853fd18505
Lsunae <sjsao5012@gmail.com>
2023-03-22 18:16:43 +0900

    로그 추가

--- app/src/main/java/com/lsunae/search_app/data/repository/video/VideoSearchRepositoryImpl.kt
+++ app/src/main/java/com/lsunae/search_app/data/repository/video/VideoSearchRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.lsunae.search_app.data.repository.video
 
+import android.util.Log
 import com.lsunae.search_app.api.VideoSearchService
 import com.lsunae.search_app.data.model.video.VideoSearchResponse
 import retrofit2.Response
@@ -12,6 +13,7 @@ class VideoSearchRepositoryImpl @Inject constructor(private val api: VideoSearch
         page: Int,
         sort: String
     ): Response<VideoSearchResponse> {
+        Log.d("[${javaClass.name}] api_video_ ", "query: $query, page: $page, sort: $sort")
         return api.searchVideo(query = query, page = page, size = 5, sort = sort)
     }
 }
